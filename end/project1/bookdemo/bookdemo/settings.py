5c46572667654b4fb88c238e821fdd5dc1563141
sqz123 <124@qq.com>
2020-02-11 16:35:38 +0800

    xiangmuvv

--- end/project1/bookdemo/bookdemo/settings.py
+++ end/project1/bookdemo/bookdemo/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'booktest',
 ]
 
 MIDDLEWARE = [
@@ -75,6 +76,8 @@ WSGI_APPLICATION = 'bookdemo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES 可以配置项目的数据库
+# 默认使用的是关系型数据库 sqlite 轻量级文件数据库 仅有一个文件db.sqlite3
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
